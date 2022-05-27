cb19b901eddc948c26ba04751bb75f25c7f845f2
Mohammed Shalan <mohammed.shalan@gizasystems.com>
2022-05-28 00:27:12 +0200

    P4- Fix click on body behaviour

--- Day4. Hidden Search Widget/script.js
+++ Day4. Hidden Search Widget/script.js
@@ -6,3 +6,10 @@ searchBtn.addEventListener('click',()=>{
     searchInput.classList.toggle('active')
     input.focus()
 })
+
+document.body.addEventListener('click',()=>{
+    searchInput.classList.remove('active')
+},true)
+// For reference:
+//https://www.w3.org/TR/DOM-Level-3-Events///#event-flow
+//https://stackoverflow.com/a/8054864
