597c3ffe7f691432e42fe6b1b47b2beb178bd6e9
Mohammed Shalan <mohammed.shalan@gizasystems.com>
2022-06-20 21:45:33 +0200

    P26- finish slider vertical movement

---
+++ Day26. Vertical Slider/script.js
@@ -0,0 +1,34 @@
+const slideLeft = document.querySelector('.left-side')
+const slideRight = document.querySelector('.right-side')
+const slidesLength = slideLeft.querySelectorAll('div').length
+const slide_up = document.querySelector('.up-btn')
+const slide_down = document.querySelector('.down-btn')
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+slide_up.addEventListener('click', () => slide('up'))
+slide_down.addEventListener('click', () => slide('down'))
+
+function slide(direction) {
+    let sliderHeight = slideLeft.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+
+    }
+
+    if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
