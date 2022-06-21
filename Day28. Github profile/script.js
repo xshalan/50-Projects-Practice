86070f611c998ac54cc2a91e07c80c4d5d931c1b
Mohammed Shalan <mohammed.shalan@gizasystems.com>
2022-06-21 08:21:46 +0200

    P28- add project- github profile

---
+++ Day28. Github profile/script.js
@@ -0,0 +1,85 @@
+const GITHUB_API_URL = "https://api.github.com/users/"
+const REPO_API = '/repos?sort=created'
+
+const searchForm = document.getElementById('form')
+
+const searchInput = document.getElementById('search')
+const card_info = document.querySelector('.card-info')
+
+async function getRepos(username) {
+    try {
+        const REPO_URL = GITHUB_API_URL + username + REPO_API
+        const { data } = await axios(REPO_URL)
+        console.log(data)
+        createRepos(data)
+    } catch (error) {
+        //error
+    }
+}
+
+
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(GITHUB_API_URL + username)
+
+        createUserCard(data)
+        getRepos(username)
+    } catch (err) {
+        // if(err.response.status == 404) {
+        //     //createErrorCard('No profile with this username')
+        // }
+        console.log(err)
+    }
+}
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = searchInput.value
+    searchInput.value = ''
+    getUser(user)
+})
+const createRepos = (data) => {
+    const reposEl = document.querySelector('.repos')
+
+    data.slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.innerText = repo.name
+            repoEl.target = '_blank'
+            reposEl.appendChild(repoEl)
+            console.log(repo.html_url)
+        })
+
+}
+const createUserCard = (data) => {
+    card_info.innerHTML = `
+    <div class="profile-img">
+            <img src="${data.avatar_url}" />
+        </div>
+
+        <div class="user-info">
+            <h2>${data.name}</h2>
+            <p>${data.bio}</p>
+            <ul>
+                <li>${data.followers} <strong>Followers</strong></li>
+
+                <li>${data.following}  <strong>Following</strong></li>
+
+                <li>${data.public_repos}  <strong>Repos</strong></li>
+            </ul>
+
+            
+            <div class="repos">
+            
+        </div>
+
+        </div>
+
+        
+
+    `
+}
