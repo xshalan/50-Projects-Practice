8e4af903107e92390df030c95bbee7f2b9bff9f0
MohammedShalan <nouralislam95@gmail.com>
2022-06-27 04:52:57 +0200

    P32- handle checkbox  js

--- Day32. Good Cheap Fast/script.js
+++ Day32. Good Cheap Fast/script.js
@@ -1,1 +1,18 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.getElementById('good')
+const cheap = document.getElementById('cheap')
+const fast = document.getElementById('fast')
 
+toggles.forEach(element => {
+    element.addEventListener('change', (e) => {
+        if (cheap.checked && good.checked && fast.checked) {
+            if (e.target === good) {
+                fast.checked = false
+            } else if (e.target === cheap) {
+                good.checked = false
+            } else if (e.target === fast) {
+                cheap.checked = false
+            }
+        }
+    })
+});
